b76e9ebb6e9b0d6e15d5c0eac4deceec148c61be
RajatBakshi1 <42149607+RajatBakshi1@users.noreply.github.com>
2018-08-13 18:50:49 +0530

    Update Leapyear.py

--- program/Leapyear.py
+++ program/Leapyear.py
@@ -1,6 +1,6 @@
 # Q1:- Take an input year from user and decide whether it is a leap year or not.
 year=int(input("Enter the year:"))
-if(year%4)==0 and (year%100)==0 and (year%400)==0:
+if(year%4)==0 or (year%100)==0 and (year%400)!=0:
     print("The entered year is the leap year:",year)
 else:
     print("The entered year is not a leap year:",year)
